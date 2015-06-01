d0ce8fd873ae76effe57019f5847718c26087e1d
mamasnicolaou <mamas.nicolaou@pinchfavor.com>
2015-05-31 21:43:18 -0400

    done

--- plumeriaESL/plumeriaESL/views.py
+++ plumeriaESL/plumeriaESL/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.http import HttpResponseRedirect
 from django.core.mail import send_mail
+from django.shortcuts import render_to_response
 
 def plumeria(request):
     template = loader.get_template('plumeria.html')
@@ -9,16 +10,17 @@ def plumeria(request):
     return HttpResponse(template.render(context))
 
 def signup(request):
-	if request.method == 'POST':
-		first_name = request.POST['form-first-name']
-       	last_name = request.POST['form-last-name']
-       	email = request.POST.get('form-email')
-       	message = request.POST.get('form-message')
-       	print 
-       	send_mail('Student Query from Plumeria', "First Name:"+
-       		first_name+"\nLast Name:"+
+  template = loader.get_template('plumeria.html')
+  context = RequestContext(request)
+  if request.method == 'POST':
+    first_name = request.POST['form-first-name']
+    last_name = request.POST['form-last-name']
+    email = request.POST.get('form-email')
+    message = request.POST.get('form-message')
+    print send_mail('Student Query from Plumeria', "First Name:"+
+      		first_name+"\nLast Name:"+
        		last_name+"\nEmail:"+
        		str(email)+"\nMessage:"+
        		str(message), str(email),
     ['mamas.nicolaou@outlook.com'], fail_silently=False)
-	return HttpResponse(status=200)
+    return render_to_response('plumeria.html', {'emailed':True}, context)
